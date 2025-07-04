e86551fa13ff2c410217344268ee16e136b7da91
Abulfitouh Abdurahman <tutuabdurahman@gmail.com>
2025-07-04 00:33:19 -0500

    changing background on user click

--- styling folder/new_file.js
+++ styling folder/new_file.js
@@ -1,7 +1,31 @@
-var button = document.querySelector('button');
-var rock = document.getElementById("ChangeMe");
+//var button = document.querySelector('button');
+//var rock = document.getElementById("ChangeMe");
 
-function changeColor(){
-    Box.style.background = "blue";
+/*function changeBG(){
+    document.body.backgroundImage = pictures[currendIndex];
+    currentIndex = (currentIndex + 1) % pictures.length;
+}*/
+
+// this is to make a button that changes the background into these images with each click//
+const pictures = [
+    'url("images/birdman.png")',
+    'url("images/rengoku.jpg")',
+    'url("images/sassy.png")',
+    'url("images/sanji.png")',
+    'url("images/zoro.png")',
+    'url("images/vegapunklilth.png")',
+    'url("images/birdmanfriend.png")',
+    'url("images/birdmanfriend3.png")',
+    'url("images/chopper.webp")',
+    'url("images/goku.webp")',
+    'url("images/luffy.jpg")'
+];
+let currentIndex = 0;
+function changeBG(){
+    //changes the background image of the body
+    document.body.style.backgroundImage = pictures[currentIndex];
+    //moves to the next image and loops back to the start if at end
+    currentIndex = (currentIndex + 1) % pictures.length;
 }
 
+console.log('hi my name is  joe and i work at a button factory!');
